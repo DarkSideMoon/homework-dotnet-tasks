2ec3982657e5f6dc68dd4c4407c5b9dd97c9680d
DarkSideMoon <16blackwood16@gmail.com>
2021-10-04 22:35:20 +0300

    Add tests and configure CI to build tests

---
+++ tests/Homework.Dotnet.Tasks.Unit.Tests/UnitTest1.cs
@@ -0,0 +1,72 @@
+using NUnit.Framework;
+
+namespace Homework.Dotnet.Tasks.Unit.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test5()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test6()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test7()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test8()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test9()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test10()
+        {
+            Assert.Pass();
+        }
+    }
+}
